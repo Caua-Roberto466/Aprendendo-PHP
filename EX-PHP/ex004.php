35cbe2dc4e9230f7b32cc70513bfa7f57571bf5d
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-09-10 15:43:22 -0300

    Create ex004.php

---
+++ EX-PHP/ex004.php
@@ -0,0 +1,11 @@
+<?php 
+$numero1 = 8;
+$numero2 = 9;
+
+$soma = $numero1 + $numero2;
+$sub = $numero1 - $numero2;
+$multi = $numero1 * $numero2;
+$div = $numero1 / $numero2;
+
+echo "A soma é $soma, a subtração é $sub, a multiplicação é $multi, a divisão é $div";
+?>
