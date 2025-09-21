56599dad6fc1070ac83c7dcb96d56de96c6ae0e0
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-09-21 18:58:28 -0300

    Exercício 13 de PHP
    Nele eu percorri um array associativo com foreach

--- EX-PHP/ex013.php
+++ EX-PHP/ex013.php
@@ -1,3 +1,8 @@
 <?php 
+//Exercício 13 - Percorrendo Array assiciativo
+$notas = array('Maria' => 5, 'Rafaella' => 9, 'Marcos' => 7, 'Alves' => 10);
 
+foreach($notas as $nome => $nota){
+    echo "$nome tirou $nota"."<br>";
+}
 ?>
