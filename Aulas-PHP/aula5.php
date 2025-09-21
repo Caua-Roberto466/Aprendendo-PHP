42f444d5a1cfc786a42c2549b016a1d2966a3d4b
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-09-21 18:51:30 -0300

    Aula 5 de PHP
    Aula 5 de php, trabalhando com arrays associativos

---
+++ Aulas-PHP/aula5.php
@@ -0,0 +1,13 @@
+<?php 
+//Arrays associativos - Utilizam strings como chave
+//As chaves estão ligadas à um valor
+//Exemplo:
+$estados = array('Am' => 'Amazonas', 'BA' => 'Bahia', 'ES' => 'Espirito Santo', 'SP' => 'São Paulo', 'TO' => 'Tocantins');
+
+//Exibindo um array associativo, você usa a chave associada para exibir
+//Exemplo:
+
+$selecionado = $estados['SP'];
+
+echo "Olá! Bem vindo à $selecionado <br>"
+?>
