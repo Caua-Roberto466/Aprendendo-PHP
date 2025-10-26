8e707614bad88c323babe94ec8cf585f5fff9ac9
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-10-26 13:11:47 -0300

    Aula 22 de PHP
    Apresentamos exemplos para gerar um número aleatório, calcular raiz quadrada, elevar números ao quadrado e ao cubo e encontrar valores absolutos. Essas adições demonstram funções matemáticas básicas do PHP.

--- EX-PHP/ex022.php
+++ EX-PHP/ex022.php
@@ -1,3 +1,29 @@
 <?php 
+//Número aleatório
 $num_aleatorio = rand(1, 100);
+echo "O número aleatório é $num_aleatorio";
+
+echo"<br><br>";
+
+//Raiz quadrada
+$raiz_quadrada = sqrt(25);
+echo "A raiz quadrada de 25 é $raiz_quadrada";
+
+echo"<br><br>";
+
+//Número ao quadrado
+$quadrado = pow(15, 2);
+echo "15 ao quadro é $quadrado";
+
+echo"<br><br>";
+
+//Número ao cubo
+$cubo = $num_aleatorio**3;
+echo "$num_aleatorio ao cubo é $cubo";
+
+echo"<br><br>";
+
+//Valor absoluto
+$absoluto = abs(-23);
+echo "O valor absoluto de -23 é $absoluto";
 ?>
