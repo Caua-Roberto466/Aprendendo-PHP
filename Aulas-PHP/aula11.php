9a622126470066b2bcd5bf56c4690b20434477ff
Caua-Roberto466 <cauaroberto211@gmail.com>
2025-11-10 16:47:35 -0600

    Começo da Aula 11

--- Aulas-PHP/aula11.php
+++ Aulas-PHP/aula11.php
@@ -1,4 +1,17 @@
 <?php 
 //Estudando as funções de data em php
+/* 
+date() - a função date() em PHP serve para formatar datas, de acordo com o formato informados, exemplo: date('d/m/Y') a saída é 10/11/2025
+*/
+echo date('d/m/Y');
+echo "<br><br>";
+echo date('y/d/m');
+echo "<br><br>";
+echo date('d/m/Y H:i:s');
 
+/* 
+
+*/
+echo "<br><br>";
+echo time()
 ?>
