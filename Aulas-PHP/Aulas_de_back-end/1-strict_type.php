323e1e88f71a116cc9129af7fc02d2fd45084ca7
Cauã Roberto <cauaroberto211@gmail.com>
2026-03-03 20:04:30 -0300

    Mais strict type em php

--- Aulas-PHP/Aulas_de_back-end/1-strict_type.php
+++ Aulas-PHP/Aulas_de_back-end/1-strict_type.php
@@ -2,12 +2,16 @@
 
 declare(strict_types=1);
 
-
+/*
 function verificar(bool $verificado) : void{
     echo $verificado ? "Verificado<br>" : "Não verificado<br>";
 }
 
 verificar(false);
 verificar(1);
-verificar(0);
+verificar(0);*/
+
+require 'user.php';
+
+persona(name: 'true');
 ?>
---
+++ Aulas-PHP/Aulas_de_back-end/user.php
@@ -0,0 +1,8 @@
+<?php
+
+function persona(String $name){
+    echo $name;
+}
+
+
+?>
