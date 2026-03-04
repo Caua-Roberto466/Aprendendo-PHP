bfaf3b9bdef6dc168eb1fde7669060c1ebb54c61
Cauã Roberto <cauaroberto211@gmail.com>
2026-03-03 21:52:43 -0300

    Escopo de funções em PHP
    Nessa aula eu entendi melhor sobre os escopos de variáveis em php

--- Aulas-PHP/Aulas_de_back-end/2-escopo_de_variaveis.php
+++ Aulas-PHP/Aulas_de_back-end/2-escopo_de_variaveis.php
@@ -1,1 +1,28 @@
 <?php
+
+/*$linguagem = 'PHP';
+
+include 'exemplo.php'; (exemplo.php:$linguagem = 'Java';)
+
+echo $linguagem;
+*/
+
+/*
+$nome = "Cauã";
+
+function apresentar() : void{
+    $nome = "Caio";
+    echo "$nome";
+}
+
+apresentar();
+echo "\n";
+echo $nome;
+*/
+
+$nome = "Carlos";
+function apresentar(){
+	global $nome;
+	echo $nome;
+} //Saída $nome
+?>
