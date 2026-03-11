93b641bcc37c6723bd791df59ad5de9eed027a70
Cauã Roberto <cauaroberto211@gmail.com>
2026-03-11 08:25:55 -0300

    Spread operator em PHP
    Nessa aula eu aprendi a usar o Spread Operator, um operador usado para descompactar arrays, e pode ser usado para juntar arrays e para passar eles como argumentos de uma função

--- Aulas-PHP/Aulas_de_back-end/5-operador_spread.php
+++ Aulas-PHP/Aulas_de_back-end/5-operador_spread.php
@@ -5,3 +5,13 @@ $operado = [...$arreyA, 5, 6, 7, 8, ...$arreyB];
 foreach($operado as $valor){
     echo "$valor ";
 }
+
+echo "<br>";
+
+$array1 = [1, 2, 3];
+$array2 = [4, 5, 6];
+
+$array3 = [$array1, ...$array2];
+print_r($array3);
+
+echo "<br>";
